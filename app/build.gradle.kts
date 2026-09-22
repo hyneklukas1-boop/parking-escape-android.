694705abc5879bf446cbdfd0a9a8d72e9ce2423c
hyneklukas1-boop <hynek.lukas1@gmail.com>
2026-09-22 19:49:40 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "cz.lakys18.parkingescape"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "cz.lakys18.parkingescape"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+}
